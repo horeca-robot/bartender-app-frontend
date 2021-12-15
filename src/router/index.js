34ffff693df737d48a94cc717894baf091fe07b4
Gilsing,Guylian Gilsing G <g.gilsing@student.fontys.nl>
2021-12-15 14:56:06 +0100

    Lazy-loaded all components to increase load times

--- src/router/index.js
+++ src/router/index.js
@@ -1,66 +1,58 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import OrderDetail from '../views/Orders/OrderDetail.vue'
-import CreateOrder from '../views/Orders/CreateOrder.vue'
-import UpdateOrder from '../views/Orders/UpdateOrder.vue'
-import ProductOverview from '../views/Products/ProductOverview.vue'
-import Robots from '../views/Robots/Robots.vue';
-import RestaurantMap from '../views/RestaurantMaps/RestaurantMap.vue'
-import EmployeeSelection from '../views/Auth/EmployeeSelection.vue'
-import Login from '../views/Auth/Login.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/orders',
-    name: 'Orders',
-    component: () => import(/* webpackChunkName: "orders" */'@/views/Orders/Orders.vue')
-  },
-  {
-    path: '/orders/create',
-    name: 'CreateOrder',
-    component: CreateOrder
-  },
-  {
-    path: '/orders/:id',
-    name: 'OrderDetail',
-    component: OrderDetail
-  },
-  {
-    path: '/orders/update/:id',
-    name: 'UpdateOrder',
-    component: UpdateOrder
-  },
-  {
-    path: '/products',
-    name: 'ProductOverview',
-    component: ProductOverview
-  },
-  {
-    path: '/map',
-    name: 'RestaurantMap',
-    component: RestaurantMap
-  },
-  {
-      path: '/robots',
-      name: 'Robots',
-      component: Robots
-  },
-  {
-    path: '/employeeSelection',
-    name: 'employeeSelection',
-    component: EmployeeSelection
-  },
-  {
-    path: '/login/:id',
-    name: 'Login',
-    component: Login
-  },
-  {
-    path: '*',
-    redirect: '/orders',
-  },
+    {
+        path: '/orders',
+        name: 'Orders',
+        component: () => import(/* webpackChunkName: "orders" */'@/views/Orders/Orders.vue')
+    },
+    {
+        path: '/orders/create',
+        name: 'CreateOrder',
+        component: () => import(/* webpackChunkName: "orders" */'@/views/Orders/CreateOrder.vue')
+    },
+    {
+        path: '/orders/:id',
+        name: 'OrderDetail',
+        component: () => import(/* webpackChunkName: "orders" */'@/views/Orders/OrderDetail.vue')
+    },
+    {
+        path: '/orders/update/:id',
+        name: 'UpdateOrder',
+        component: () => import(/* webpackChunkName: "orders" */'@/views/Orders/UpdateOrder.vue')
+    },
+    {
+        path: '/products',
+        name: 'ProductOverview',
+        component: () => import(/* webpackChunkName: "products" */'@/views/Products/ProductOverview.vue')
+    },
+    {
+        path: '/map',
+        name: 'RestaurantMap',
+        component: () => import(/* webpackChunkName: "map" */'@/views/RestaurantMaps/RestaurantMap.vue')
+    },
+    {
+        path: '/robots',
+        name: 'Robots',
+        component: () => import(/* webpackChunkName: "robots" */'@/views/Robots/Robots.vue')
+    },
+    {
+        path: '/employeeSelection',
+        name: 'employeeSelection',
+        component: () => import(/* webpackChunkName: "auth" */'@/views/Auth/EmployeeSelection.vue')
+    },
+    {
+        path: '/login/:id',
+        name: 'Login',
+        component: () => import(/* webpackChunkName: "auth" */'@/views/Auth/Login.vue')
+    },
+    {
+        path: '*',
+        redirect: '/orders',
+    },
 ]
 
 const router = new VueRouter({
